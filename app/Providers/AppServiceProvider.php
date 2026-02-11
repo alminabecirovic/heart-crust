64d55481e62542d4da6227ff8c88d40ae13faf46
inaalmb <daisy.mimv@gmail.com>
2026-02-11 15:04:58 +0100

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,8 +9,7 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        // Форсирај HTTPS у продукцији
-        if ($this->app->environment('production')) {
+        if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
     }
